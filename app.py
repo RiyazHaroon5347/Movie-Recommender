98ebcc7b7b6edb98ed7b4a83df50cb6059e49013
Mohamed Riyaz M <144546315+RiyazHaroon5347@users.noreply.github.com>
2025-07-07 19:46:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,36 @@ def add_bg_from_url():
 
 
 @st.cache_data
+
+
+'''def load_data():
+    movies = pd.read_csv('tmdb_5000_movies.csv')
+    credits = pd.read_csv('tmdb_5000_credits.csv')
+    movies = movies.merge(credits, on='title')
+    movies = movies[['movie_id', 'title', 'overview', 'genres', 'keywords', 'cast', 'crew']]
+    movies.dropna(inplace=True)
+
+    def convert(obj):
+        return [i['name'] for i in ast.literal_eval(obj)]
+
+    def get_top_cast(obj):
+        return [i['name'] for i in ast.literal_eval(obj)[:3]]
+
+    def get_director(obj):
+        return [i['name'] for i in ast.literal_eval(obj) if i['job'] == 'Director'][:1]
+
+    movies['genres'] = movies['genres'].apply(convert)
+    movies['keywords'] = movies['keywords'].apply(convert)
+    movies['cast'] = movies['cast'].apply(get_top_cast)
+    movies['crew'] = movies['crew'].apply(get_director)
+    movies['overview'] = movies['overview'].apply(lambda x: x.split())
+    movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + movies['cast'] + movies['crew']
+
+    new_df = movies[['movie_id', 'title', 'tags']]
+    new_df['tags'] = new_df['tags'].apply(lambda x: " ".join(x)).str.lower()
+
+    return new_df'''
+
 def load_data():
     movies = pd.read_csv('tmdb_5000_movies.csv')
     credits = pd.read_csv('tmdb_5000_credits.csv')
@@ -109,86 +139,11 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 
-
-
 movie_list = new_df['title'].sort_values().tolist()
 selected_movie = st.selectbox("Choose a movie", movie_list)
 
 
-
 TMDB_API_KEY = "9b12d347b6ae32fa5fe10efc7d58c7a3"
-
-
-
-
-def fetch_popular_movies(page=1):
-    url = f"https://api.themoviedb.org/3/movie/popular?api_key={API_KEY}&language=en-US&page={page}"
-    response = requests.get(url)
-    return response.json()['results']
-
-def fetch_credits(movie_id):
-    url = f"https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key={API_KEY}"
-    return requests.get(url).json()
-
-def fetch_keywords(movie_id):
-    url = f"https://api.themoviedb.org/3/movie/{movie_id}/keywords?api_key={API_KEY}"
-    return requests.get(url).json()
-
-def build_movie_data(pages=1):
-    movies_list = []
-    credits_list = []
-
-    for page in range(1, pages + 1):
-        for movie in fetch_popular_movies(page):
-            movie_id = movie['id']
-            title = movie['title']
-            overview = movie.get('overview', '')
-
-            # Genres
-            genres = [{'id': g['id'], 'name': g['name']} for g in movie.get('genre_ids', [])]
-
-            # Credits
-            credit_data = fetch_credits(movie_id)
-            cast = credit_data.get('cast', [])
-            crew = credit_data.get('crew', [])
-
-            # Keywords
-            keyword_data = fetch_keywords(movie_id)
-            keywords = keyword_data.get('keywords', [])
-
-            # Format
-            movie_row = {
-                'movie_id': movie_id,
-                'title': title,
-                'overview': overview,
-                'genres': str(genres),
-                'keywords': str(keywords),
-                'cast': str(cast),
-                'crew': str(crew)
-            }
-
-            movies_list.append(movie_row)
-            credits_list.append({
-                'movie_id': movie_id,
-                'title': title,
-                'cast': str(cast),
-                'crew': str(crew)
-            })
-
-    return pd.DataFrame(movies_list), pd.DataFrame(credits_list)
-
-movies_df, credits_df = build_movie_data(pages=2)  # 2 pages = 40 movies
-movies_df.to_csv("new_movies.csv", index=False)
-credits_df.to_csv("new_credits.csv", index=False)
-
-existing_movies = pd.read_csv("movies.csv")
-existing_credits = pd.read_csv("credits.csv")
-
-combined_movies = pd.concat([existing_movies, movies_df]).drop_duplicates(subset='movie_id')
-combined_credits = pd.concat([existing_credits, credits_df]).drop_duplicates(subset='movie_id')
-
-combined_movies.to_csv("movies.csv", index=False)
-combined_credits.to_csv("credits.csv", index=False)
 
 
 if st.button("Recommend"):
